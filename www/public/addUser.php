6246a7e0aa72d19e4d48ad711c7ed77136d0f218
ErikKL06 <06kler@skola.engelholm.se>
2024-12-14 23:18:27 +0100

    börja på inlogg

---
+++ www/public/addUser.php
@@ -0,0 +1,26 @@
+<?php    
+if(isset($_POST['email'],$_POST['userName'],$_POST['pwd'])){                
+    include_once('inc/db.inc.php');    
+    
+    $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_SPECIAL_CHARS);
+    $user = filter_input(INPUT_POST, 'userName', FILTER_SANITIZE_SPECIAL_CHARS);
+    $pwd = password_hash($_POST['pwd'], PASSWORD_DEFAULT);
+    
+    /* Bygger upp sql frågan */
+    $stmt= $db->prepare("INSERT INTO user(uid, firstname, surname, username, password) VALUES(UUID(), :fn, :sn,:user,:pwd)");
+    
+    $stmt->bindValue(":fn", $email);
+    $stmt->bindValue(":user", $user);
+    $stmt->bindValue(":pwd", $pwd);
+    
+    // Om INSERT gick bra! Om man användarnamn är upptaget fungerar inte insert
+    try{
+        $stmt->execute();
+        header('Location: addUserForm.html'); // Borde visa att allt gick bra!
+    }catch(Exception $e){
+        header('Content-Type: text/html; charset=utf-8');
+        echo "<p>Kunde inte lägga till användaren. Kontrollera användarnamnet</p>";
+        echo "<a href = 'addUserForm.html'>Försök igen</a>";
+    }
+}
+?>
