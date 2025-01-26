4c15f270926f7b372f0b97f09e796951920d289f
ErikKL06 <06kler@skola.engelholm.se>
2025-01-26 16:12:15 +0100

    fixat intervall

--- www/public/game3.js
+++ www/public/game3.js
@@ -60,7 +60,7 @@ window.onload = function () {
     document.addEventListener("keyup", changeDirection);
 
     // Call the update function every 100 milliseconds to update the game state
-    setInterval(update, 1000 / 8);
+    setInterval(update, 1000 / 6);
     re.addEventListener("click", restart);
 
 }
