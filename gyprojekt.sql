51eb22abb2248c1596197670c373a1a6a5ea102b
ErikKL06 <148197480+ErikKL06@users.noreply.github.com>
2025-04-29 09:41:55 +0200

    Add files via upload

--- gyprojekt.sql
+++ gyprojekt.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: mariadb
--- Generation Time: Mar 21, 2025 at 11:38 AM
+-- Generation Time: Apr 29, 2025 at 07:41 AM
 -- Server version: 11.5.2-MariaDB-ubu2404
 -- PHP Version: 8.2.8
 
@@ -24,6 +24,18 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `scores`
+--
+
+CREATE TABLE `scores` (
+  `sid` int(10) UNSIGNED NOT NULL,
+  `uid` char(36) NOT NULL,
+  `score` int(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
@@ -36,8 +48,23 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_swedish_ci;
 
 --
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`uid`, `username`, `email`, `password`, `highscore`) VALUES
+('1616003e-0e25-11f0-a19a-0242ac141003', 'vice06', '06jovi@gmail.com', '$2y$12$Ww3251.TX3AUjjtQBOl2R.KgLNojfLJ08sEnwxPMSFUpl//D6FW2q', 0),
+('d979a744-0bbf-11f0-a19a-0242ac141003', 'ida1', 'wedbergida@gmail.com', '$2y$12$rnGqa1IaDhxMChPI4LqMF.NjKb4/9jr23VqVuri/cJLw8M7MgRRg2', 0);
+
+--
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `scores`
+--
+ALTER TABLE `scores`
+  ADD PRIMARY KEY (`sid`),
+  ADD KEY `uid` (`uid`);
 
 --
 -- Indexes for table `users`
@@ -46,6 +73,26 @@ ALTER TABLE `users`
   ADD PRIMARY KEY (`uid`),
   ADD UNIQUE KEY `username` (`username`),
   ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `scores`
+--
+ALTER TABLE `scores`
+  MODIFY `sid` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `scores`
+--
+ALTER TABLE `scores`
+  ADD CONSTRAINT `scores_ibfk_1` FOREIGN KEY (`uid`) REFERENCES `users` (`uid`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
